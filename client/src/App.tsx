700fd5cf61c7ace9b04e4a9f2e5faf8daf166dc6
Eric H <eric.knows@yahoo.com>
2026-07-26 10:44:13 -0500

    Apply shared shell to core app pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AppShell } from "./components/AppShell";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -25,10 +26,10 @@ function Router() {
       <Route path={"/signup"} component={Signup} />
       <Route path={"/forgot-password"} component={ForgotPassword} />
       <Route path={"/onboarding/organization"}><ProtectedRoute><OnboardingOrganization /></ProtectedRoute></Route>
-      <Route path={"/app/workforce"}><ProtectedRoute requireOrg><Workforce /></ProtectedRoute></Route>
-      <Route path={"/app"}><ProtectedRoute requireOrg><AppDashboard /></ProtectedRoute></Route>
+      <Route path={"/app/workforce"}><ProtectedRoute requireOrg><AppShell><Workforce /></AppShell></ProtectedRoute></Route>
+      <Route path={"/app"}><ProtectedRoute requireOrg><AppShell><AppDashboard /></AppShell></ProtectedRoute></Route>
       <Route path={"/app/deploy"}><ProtectedRoute requireOrg><DeployV2 /></ProtectedRoute></Route>
-      <Route path={"/app/connections"}><ProtectedRoute requireOrg><Connections /></ProtectedRoute></Route>
+      <Route path={"/app/connections"}><ProtectedRoute requireOrg><AppShell><Connections /></AppShell></ProtectedRoute></Route>
       <Route path={"/app/settings/profile"}><ProtectedRoute requireOrg><AccountSettings /></ProtectedRoute></Route>
       <Route path={"/app/settings/team"}><ProtectedRoute requireOrg><AccountSettings /></ProtectedRoute></Route>
       <Route path={"/app/settings/organization"}><ProtectedRoute requireOrg><AccountSettings /></ProtectedRoute></Route>
