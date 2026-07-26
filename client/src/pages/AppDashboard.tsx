88293306633127f7ee60a255981824c0950dde73
Eric H <eric.knows@yahoo.com>
2026-07-26 10:38:21 -0500

    Remove Today account overlay

--- client/src/pages/AppDashboard.tsx
+++ client/src/pages/AppDashboard.tsx
@@ -1,5 +1,4 @@
 import Today from "./Today";
-import { AppUserMenu } from "@/components/AppUserMenu";
 
 export default function AppDashboard() {
   return (
@@ -8,17 +7,8 @@ export default function AppDashboard() {
         .app-dashboard-shell header {
           box-shadow: none !important;
         }
-
-        @media (min-width: 640px) {
-          .app-dashboard-shell header > .container > a:last-child {
-            margin-right: 230px;
-          }
-        }
       `}</style>
       <Today />
-      <div className="fixed right-4 top-2 z-[70] sm:right-6">
-        <AppUserMenu />
-      </div>
     </div>
   );
 }
