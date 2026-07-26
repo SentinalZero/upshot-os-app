526774d6d5512f472e16efe1a7c466d597d062ce
Eric H <eric.knows@yahoo.com>
2026-07-25 19:44:58 -0500

    Reserve header space for account menu

--- client/src/pages/AppDashboard.tsx
+++ client/src/pages/AppDashboard.tsx
@@ -8,8 +8,17 @@ export default function AppDashboard() {
         .app-dashboard-shell header {
           box-shadow: none !important;
         }
+
+        @media (min-width: 640px) {
+          .app-dashboard-shell header > .container > a:last-child {
+            margin-right: 230px;
+          }
+        }
       `}</style>
-      <Today accountMenu={<AppUserMenu />} />
+      <Today />
+      <div className="fixed right-4 top-2 z-[70] sm:right-6">
+        <AppUserMenu />
+      </div>
     </div>
   );
 }
